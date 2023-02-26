c4179ca5e07c97810a2873518a1964313c0dfaad
Pablo Merchán-Rivera <81840758+mr-pablinho@users.noreply.github.com>
2023-02-26 19:19:10 +0100

    Refactor script and add app class

--- __main__.py
+++ __main__.py
@@ -3,84 +3,74 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-# Define a function to import the data set
-def import_data():
-    return pd.read_excel("../data/dummy_data.xlsx")
+class ExpenseDashboard:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.df = None
+        self.grouped_df = None
+        self.total_by_category = None
+        self.melted_data = None
+        self.category_colors = {
+            "Food": "orange",
+            "Travel": "blue",
+            "Entertainment": "green",
+            "Shopping": "purple",
+            "Health": "red",
+            "Housing": "gray",
+            "Transportation": "brown"
+        }
 
-# Define a function to preprocess the data set
-def preprocess_data(df):
-    # Group the expenses by date and category and sum the amounts
-    grouped_df = df.groupby(["Date", "Category"])["Amount"].sum(numeric_only=True).reset_index()
+    def import_data(self):
+        self.df = pd.read_excel(self.file_path)
 
-    # Add a new column with the month of each expense
-    grouped_df["Month"] = pd.to_datetime(grouped_df["Date"]).dt.to_period("M")
+    def preprocess_data(self):
+        self.grouped_df = self.df.groupby(["Date", "Category"])["Amount"].sum(numeric_only=True).reset_index()
 
-    # Compute the total amount of expenses by category and by month
-    total_by_category_and_month = pd.pivot_table(grouped_df, values="Amount", index="Month", columns="Category", aggfunc=np.sum)
+        self.grouped_df["Month"] = pd.to_datetime(self.grouped_df["Date"]).dt.to_period("M")
 
-    # Compute the total amount of expenses by category
-    total_by_category = grouped_df.groupby("Category")["Amount"].sum().reset_index()
+        total_by_category_and_month = pd.pivot_table(self.grouped_df, values="Amount", index="Month", columns="Category", aggfunc=np.sum)
 
-    # Compute the total amount of expenses by month and by category
-    total_by_month_and_category = pd.pivot_table(grouped_df, values="Amount", index="Category", columns="Month", aggfunc=np.sum)
+        self.total_by_category = self.grouped_df.groupby("Category")["Amount"].sum().reset_index()
 
-    # Reshape the data using the melt function to create a flat list of values
-    melted_data = total_by_month_and_category.reset_index().melt(id_vars=["Category"], var_name="Month", value_name="Amount")
+        total_by_month_and_category = pd.pivot_table(self.grouped_df, values="Amount", index="Category", columns="Month", aggfunc=np.sum)
 
-    # Convert the 'Month' column to a string representation of the month
-    melted_data["Month"] = melted_data["Month"].dt.strftime("%b %Y")
+        self.melted_data = total_by_month_and_category.reset_index().melt(id_vars=["Category"], var_name="Month", value_name="Amount")
 
-    return grouped_df, total_by_category, melted_data
+        self.melted_data["Month"] = self.melted_data["Month"].dt.strftime("%b %Y")
 
-# Define a function to create the Plotly bar plots
-def create_plots(grouped_df, total_by_category, melted_data):
-    # Create a dictionary to map each category to a color
-    category_colors = {
-        "Food": "orange",
-        "Travel": "blue",
-        "Entertainment": "green",
-        "Shopping": "purple",
-        "Health": "red",
-        "Housing": "gray",
-        "Transportation": "brown"
-    }
+    def create_daily_expenses_plot(self):
+        fig = px.bar(self.grouped_df, x="Date", y="Amount", color="Category",
+                     title="Daily Expenses by Category", text="Amount",
+                     color_discrete_map=self.category_colors)
+        return fig
 
-    # Create an interactive bar plot of the expenses
-    fig1 = px.bar(grouped_df, x="Date", y="Amount", color="Category",
-                 title="Daily Expenses by Category", text="Amount",
-                 color_discrete_map=category_colors)
+    def create_monthly_expenses_by_category_plot(self):
+        fig = px.bar(self.total_by_category, x="Category", y="Amount",
+                      title="Monthly Expenses by Category",
+                      text="Amount", color="Category",
+                      color_discrete_map=self.category_colors)
+        return fig
 
-    # Create an interactive bar plot of the monthly expenses by category
-    fig2 = px.bar(total_by_category, x="Category", y="Amount",
-                  title="Monthly Expenses by Category",
-                  text="Amount", color="Category",
-                  color_discrete_map=category_colors)
+    def create_monthly_expenses_by_month_and_category_plot(self):
+        fig = px.bar(self.melted_data, x="Month", y="Amount", color="Category",
+                      title="Monthly Expenses by Month and Category",
+                      text="Amount", color_discrete_map=self.category_colors)
+        return fig
 
-    # Create an interactive bar plot of the monthly expenses by month
-    fig3 = px.bar(melted_data, x="Month", y="Amount", color="Category",
-                  title="Monthly Expenses by Month and Category",
-                  text="Amount", color_discrete_map=category_colors)
+    def run(self):
+        self.import_data()
+        self.preprocess_data()
 
-    return fig1, fig2, fig3
+        daily_expenses_fig = self.create_daily_expenses_plot()
+        monthly_expenses_by_category_fig = self.create_monthly_expenses_by_category_plot()
+        monthly_expenses_by_month_and_category_fig = self.create_monthly_expenses_by_month_and_category_plot()
 
-# Define the main function for the Streamlit app
-def main():
-    # Set the page title and heading
-    st.set_page_config(page_title="Expense Dashboard", page_icon=":dollar:", layout="wide")
-    st.title("Expense Dashboard")
+        st.set_page_config(page_title="Expense Dashboard", page_icon=":dollar:")
+        st.title("Expense Dashboard")
+        st.plotly_chart(daily_expenses_fig, use_container_width=True)
+        st.plotly_chart(monthly_expenses_by_category_fig, use_container_width=True)
+        st.plotly_chart(monthly_expenses_by_month_and_category_fig, use_container_width=True)
 
-    # Import the data set and preprocess the data
-    df = import_data()
-    grouped_df, total_by_category, melted_data = preprocess_data(df)
-
-    # Create the Plotly bar plots
-    fig1, fig2, fig3 = create_plots(grouped_df, total_by_category, melted_data)
-
-    # Add the Plotly bar plots to the app
-    st.plotly_chart(fig1, use_container_width=True)
-    st.plotly_chart(fig2, use_container_width=True)
-    st.plotly_chart(fig3, use_container_width=True)
-
-# Call the main function to run the Streamlit app
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    dashboard = ExpenseDashboard("../data/dummy_data.xlsx")
+    dashboard.run()
